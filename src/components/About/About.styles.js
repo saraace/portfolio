485c9f40d40c7b308efecfcd3ce59128caf5e796
Sara Acevedo <sara.g.acevedo@gmail.com>
2021-09-06 16:21:17 -0400

    Rough about page layout and new favicon

---
+++ src/components/About/About.styles.js
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+
+export const Flex = styled.div`
+  display: flex;
+
+  & > div {
+    flex: 0 0 50%;
+  }
+`;
+export const Image = styled.div``;
+
+export const Content = styled.div``;
