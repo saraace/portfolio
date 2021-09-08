9cb6ef6e58b56b714c05af0b819588b51c7d25bd
Sara Acevedo <sara.g.acevedo@gmail.com>
2021-09-08 18:35:58 -0400

    Added site Title

--- src/containers/Layout/Layout.jsx
+++ src/containers/Layout/Layout.jsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { useRouter } from "next/dist/client/router";
 import Header from "./Header/Header";
 
@@ -5,6 +6,9 @@ const Layout = ({ children }) => {
   const router = useRouter();
   return (
     <div>
+      <Head>
+        <title>Sara Acevedo | Front End Engineer</title>
+      </Head>
       {router.pathname !== "/" && <Header />}
       {children}
     </div>
