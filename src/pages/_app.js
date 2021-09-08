51218a61628ebe4c576f269a5e900d90cd9006ee
Sara Acevedo <sara.g.acevedo@gmail.com>
2021-09-08 18:53:07 -0400

    Adding Google Analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,30 @@
+import { useEffect } from "react";
+import { useRouter } from "next/dist/client/router";
 import { ApolloProvider } from "@apollo/client";
 import { useApollo } from "../apollo";
 import Layout from "../containers/Layout/Layout";
+import * as ga from "../lib/ga";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   const client = useApollo(pageProps.initialApolloState);
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url);
+    };
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <ApolloProvider {...{ client }}>
       <Layout>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,6 +11,23 @@ class MyDocument extends Document {
         <Head title="Sara Acevedo | Front End Engineer">
           <meta charSet="utf-8" />
           <link rel="shortcut icon" href="/favicon.ico" />
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400&family=Playfair+Display:ital,wght@0,400;0,700;1,400;1,700&display=swap"
